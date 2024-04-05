2d4f99eb91873a20906ce37c75e626d1a02d0c91
Pablocrg <pcorrg00@estudiantes.unileon.es>
2024-04-05 20:18:42 +0200

    RAG system v1

---
+++ RagDocGPT/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from operator import itemgetter
+
+app = Flask(__name__)
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL = "gpt-3.5-turbo"
+
+model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=MODEL)
+embeddings = OpenAIEmbeddings()
+parser = StrOutputParser()
+
+template = """
+Actua como si fueres un asistente de primeros auxilios. Responde a las instrucciones fijándote únicamente en el contexto dato. Si no puedes responder a un instrucción simplemente di "No lo sé"
+Quiero que la respuesta sea un json_object con tres pares clave-valor. La primera clave será respuesta_doctor y contendrá la respuesta a la pregunta. La segunda clave será expresion_facial y contendrá una expresión facial de la siguiente lista: smile, sad, angry, surprised, funnyFace y default. Asocia la respuesta_doctor a la expresión facial que consideres más adecuada. La tercera clave animacion y será un valor de la siguiente lista: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified y Angry. Asocia la respuesta_doctor a la animación que consideres más adecuada.
+
+Contexto: {context}
+
+Pregunta: {question}
+"""
+prompt = PromptTemplate.from_template(template)
+
+loader = PyPDFLoader("primeros_auxilios_SAMUR.pdf")
+pages = loader.load_and_split()
+
+vectorstore = DocArrayInMemorySearch.from_documents(pages, embedding=embeddings)
+retriever = vectorstore.as_retriever()
+
+chain = (
+    {
+        "context": itemgetter("question") | retriever,
+        "question": itemgetter("question"),
+    }
+    | prompt
+    | model
+    | parser
+)
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.json
+    question = data.get('question')
+    if not question:
+        return jsonify({"error": "Question not provided"}), 400
+
+    result = None
+    result = chain.invoke({"question": question})
+    
+    if result is None:
+        return jsonify({"error": "No response generated"}), 500
+
+    return result
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
